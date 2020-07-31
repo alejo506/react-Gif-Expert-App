65c39ad6d97844d2c6080e6589d3362fb081abb6
Alejandro CM <alejo506a@gmail.com>
2020-07-30 22:27:28 -0600

    docs agregado

---
+++ docs/precache-manifest.18069dcaf067b0f7e75acc1de90445cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e408dc4cf3105cb1e94d360d0169a51c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bc65bd2aedb3bd286ce",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8d85e2f921451d525f0c",
+    "url": "/static/css/main.6de69940.chunk.css"
+  },
+  {
+    "revision": "5bc65bd2aedb3bd286ce",
+    "url": "/static/js/2.40cd2515.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40cd2515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d85e2f921451d525f0c",
+    "url": "/static/js/main.48c57c8a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
